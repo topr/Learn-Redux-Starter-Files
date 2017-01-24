0dcf7665ce57d159f3c1959c44ba9f5079689282
Tomasz Przybysz <tomasz.przybysz@gmail.com>
2017-01-24 19:11:52 +0000

    lesson 4: set up React Router

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
 import { render } from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 // Import CSS
 import css from './styles/style.styl';
 
 // Import components
 import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
 
-render(<Main/>, document.getElementById('root'));
+const router = (
+    <Router history={browserHistory}>
+        <Route path="/" component={Main}>
+            <IndexRoute component={PhotoGrid}/>
+            <Route path="/view/:postId" component={Single}/>
+        </Route>
+    </Router>
+);
+
+render(router, document.getElementById('root'));
